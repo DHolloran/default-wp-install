4e27d31a58ba8f093d30fcae4c6dff68ddbffe89
Dan Holloran <dtholloran@gmail.com>
2012-10-01 10:16:23 -0500

    added easy start to ajax functionality

--- wp-content/themes/fm-child-theme/_assets/inc/functions/ajax_helper.inc.php
+++ wp-content/themes/fm-child-theme/_assets/inc/functions/ajax_helper.inc.php
@@ -13,6 +13,7 @@
 
 	} // fm_is_ajax()
 
+
 // == Return Action ==============================================================
 	function fm_ajax_return_action($return_no_ajax='', $return_ajax='')
 	{
@@ -26,5 +27,29 @@
 	} // fm_ajax_return_action($return_no_ajax='', $return_ajax='')
 
 
+// == Add Ajax ==================================================================
+	function fm_add_ajax()
+	{
+		// Setup AJAX Script
+		wp_register_script('ajax-script', get_stylesheet_directory_uri().'/_assets/js/coffee/ajax.js', array('jquery'), 1.0);
+		wp_enqueue_script(  'ajax-script' );
 
+		// Localize AJAX Script
+		wp_localize_script( 'ajax-script', 'ajax_object', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) ); // setting ajaxurl
+
+		// Some Action
+		add_action( 'wp_ajax_some_action', 'fm_ajax_some_action' ); // ajax for logged in users
+		add_action( 'wp_ajax_nopriv_some_action', 'fm_ajax_some_action' ); // ajax for not logged in users
+
+	} // fm_add_ajax()
+	add_action('init', 'fm_add_ajax');
+
+
+// == Ajax Action == some_action ================================================
+	function fm_ajax_some_action() {
+		// Do some magic
+		// Send back to JS
+		echo json_encode('works');
+		die();
+	} // fm_ajax_some_action()
 
