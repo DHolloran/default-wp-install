d1b430f6fd36bcbb6eb5383c95e1ac5c76978ca4
Dan Holloran <dtholloran@gmail.com>
2012-10-01 10:19:58 -0500

    made the slideshow custom post type into slide

--- wp-content/themes/fm-child-theme/_assets/inc/admin/custom_post_types.inc.php
+++ wp-content/themes/fm-child-theme/_assets/inc/admin/custom_post_types.inc.php
@@ -19,7 +19,7 @@
 	//     'parent_item_colon' => '',
 	//     'menu_name' => __('Customs', 'your_text_domain')
 	//   );
-
+	//	 // Setup the custom post type arguments
 	//   $args = array(
 	//     'labels' => $labels,
 	//     'description' => 'This is a custom post type',
@@ -39,32 +39,34 @@
 	//     'menu_position' => null,
 	//     'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
 	//   );
+	//   // Register the Customs post type
 	//   register_post_type('custom', $args);
 	// } // create_custom_post_type_test()
 	// add_action( 'init', 'create_custom_post_type_test' );
 
 
-// == Slideshow Post Type ==================================================
-	function create_custom_post_type_slideshow()
+// == Slides Post Type ==================================================
+	function create_custom_post_type_slide()
 	{
 
-		// Setup the slideshow post type labels
+		// Setup the slide post type labels
 		$labels = array(
-	    'name' => _x('Slideshows', 'post type general name', 'your_text_domain'),
-	    'singular_name' => _x('Slideshow', 'post type singular name', 'your_text_domain'),
-	    'add_new' => _x('Add New', 'slideshow', 'your_text_domain'),
-	    'add_new_item' => __('Add New Slideshow', 'your_text_domain'),
-	    'edit_item' => __('Edit Slideshow', 'your_text_domain'),
-	    'new_item' => __('New Slideshow', 'your_text_domain'),
-	    'all_items' => __('All Slideshows', 'your_text_domain'),
-	    'view_item' => __('View Slideshow', 'your_text_domain'),
-	    'search_items' => __('Search Slideshows', 'your_text_domain'),
-	    'not_found' =>  __('No slideshows found', 'your_text_domain'),
-	    'not_found_in_trash' => __('No slideshows found in Trash', 'your_text_domain'),
+	    'name' => _x('Slides', 'post type general name', 'your_text_domain'),
+	    'singular_name' => _x('Slide', 'post type singular name', 'your_text_domain'),
+	    'add_new' => _x('Add New', 'slide', 'your_text_domain'),
+	    'add_new_item' => __('Add New Slide', 'your_text_domain'),
+	    'edit_item' => __('Edit Slides', 'your_text_domain'),
+	    'new_item' => __('New Slide', 'your_text_domain'),
+	    'all_items' => __('All Slides', 'your_text_domain'),
+	    'view_item' => __('View Slides', 'your_text_domain'),
+	    'search_items' => __('Search Slides', 'your_text_domain'),
+	    'not_found' =>  __('No slides found', 'your_text_domain'),
+	    'not_found_in_trash' => __('No slides found in Trash', 'your_text_domain'),
 	    'parent_item_colon' => '',
-	    'menu_name' => __('Slideshows', 'your_text_domain')
+	    'menu_name' => __('Slides', 'your_text_domain')
 	  );
 
+		// Setup the custom post type arguments
 	  $args = array(
 	    'labels' => $labels,
 	    'description' => 'Add your slides to the slideshow',
@@ -75,15 +77,18 @@
 	    'show_in_nav_menus'	=>TRUE,
 	    'show_in_admin_bar'	=>	TRUE,
 	    'query_var' => true,
-	    'rewrite' => array( 'slug' => _x( 'slideshow', 'URL slug', 'your_text_domain' ) ),
+	    'rewrite' => array( 'slug' => _x( 'slide', 'URL slug', 'your_text_domain' ) ),
 	    'capability_type' => 'post',
 	    'has_archive' => true,
 	    'hierarchical' => false,
-	    'menu_icon'	=>	get_stylesheet_directory_uri(). '/_assets/img/admin/icons/slideshow.png',
+	    'menu_icon'	=>	get_stylesheet_directory_uri(). '/_assets/img/admin/icons/slides.png',
 	    'capability_type'	=>	'page',
 	    'menu_position' => null,
 	    'supports' => array( 'title', 'editor', 'thumbnail' )
 	  );
-	  register_post_type('slideshow', $args);
-	} // create_custom_post_type_slideshow()
-	add_action( 'init', 'create_custom_post_type_slideshow' );
+
+	  // Register the Slides post type
+	  register_post_type('slide', $args);
+
+	} // create_custom_post_type_slide()
+	add_action( 'init', 'create_custom_post_type_slide' );
