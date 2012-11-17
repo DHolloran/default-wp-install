9ba75adec371620abe9ff0742206719827aebb66
Dan Holloran <dtholloran@gmail.com>
2012-11-17 10:52:50 -0600

    implemented sticky footer and created simple extendable button classes

---
+++ wp-content/themes/fm-child-theme/front-page.php
@@ -0,0 +1,142 @@
+<?php get_header(); ?>
+<!-- Small Buttons -->
+<ul class="pull-left button-lists">
+	<li class="clearfix">
+			<a href="#" title="" class="button gold small">Gold</a>
+	</li>
+	<li class="clearfix">
+			<a href="#" title="" class="button cyan small">Cyan</a>
+	</li>
+	<li class="clearfix">
+			<a href="#" title="" class="button purple small">Purple</a>
+	</li>
+	<li class="clearfix">
+			<a href="#" title="" class="button rosy small">Rosy</a>
+	</li>
+	<li class="clearfix">
+			<a href="#" title="" class="button green small">Green</a>
+	</li>
+	<li class="clearfix">
+			<a href="#" title="" class="button pink small">Pink</a>
+	</li>
+	<li class="clearfix">
+			<a href="#" title="" class="button blue small">Blue</a>
+	</li>
+	<li class="clearfix">
+			<a href="#" title="" class="button yellow small">Yellow</a>
+	</li>
+	<li class="clearfix">
+			<a href="#" title="" class="button magenta small">Magenta</a>
+	</li>
+	<li class="clearfix">
+			<a href="#" title="" class="button orange small">Orange</a>
+	</li>
+	<li class="clearfix">
+			<a href="#" title="" class="button red small">Red</a>
+	</li>
+	<li class="clearfix">
+			<a href="#" title="" class="button grey small">Grey</a>
+	</li>
+	<li class="clearfix">
+			<a href="#" title="" class="button black small">Black</a>
+	</li>
+	<li class="clearfix">
+			<a href="#" title="" class="button white small">White</a>
+	</li>
+</ul>
+<!-- END Small buttons -->
+
+<!-- Medium -->
+<ul class="pull-left button-lists">
+	<li class="clearfix">
+		<a href="#" title="" class="button gold medium">Gold</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button cyan medium">Cyan</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button purple medium">Purple</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button rosy medium">Rosy</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button green medium">Green</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button pink medium">Pink</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button blue medium">Blue</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button yellow medium">Yellow</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button magenta medium">Magenta</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button orange medium">Orange</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button red medium">Red</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button grey medium">Grey</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button black medium">Black</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button white medium">White</a>
+	</li>
+</ul>
+<!-- END Medium -->
+
+<!-- Large -->
+<ul class="pull-left button-lists">
+	<li class="clearfix">
+		<a href="#" title="" class="button gold large">Gold</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button cyan large">Cyan</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button purple large">Purple</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button rosy large">Rosy</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button green large">Green</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button pink large">Pink</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button blue large">Blue</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button yellow large">Yellow</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button magenta large">Magenta</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button orange large">Orange</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button red large">Red</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button grey large">Grey</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button black large">Black</a>
+	</li>
+	<li class="clearfix">
+		<a href="#" title="" class="button white large">White</a>
+	</li>
+</ul>
+
+<?php get_footer();
